6a42c99f1febd0334de3b506c7b8d4c54dd18f92
Dragos <diaconescu.dragos28@gmail.com>
2021-11-15 16:43:02 +0200

    adding header component and lins to home,shop page and contact

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.scss';
 import Homepage from '../src/pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import {Routes as Switch, Route } from 'react-router-dom'
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+    <Header/>
     <Switch>
       <Route exact path='/e-commerce-react-app' element={<Homepage/>} />
       <Route path='/e-commerce-react-app/shop' element={<ShopPage/>} />
